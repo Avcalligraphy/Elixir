d68aba2bbb2b374791a4de4df9ef0a6dac228876
name <your mail>
2022-06-12 06:11:33 +0700

    first commit

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from "../images/house-1.jpg";
+import ImageTwo from "../images/house-2.jpg";
+
+export const InfoData = {
+  heading: "Expolore our beutiful homes",
+  paragraphOne:
+    "Dapur sederhana akan terlihat aesthetic dan berfungsi dengan baik, bila kamu jeli menata dekorasinya. Keinginan mewujudkan dapur minimalis modern ukuran kecil tapi cantik, bisa kamu mulai dari bagian dinding.Bisa dimulai memilih warna cat dinding terbaik, hingga menggantung hiasan guna melengkapi dekorasi dapur dapur sederhana. Namun, untuk bikin desain dapur cantik, tidak harus selalu mengandalkan warna cat. Kamu dapat mengeksplorasi desain dapur minimalis 3x2 meter terlihat menarik dengan mengaplikasikan dinding batu bata yang diekspos.Langkah itu pastinya, bakal mendatangkan suasana selaiknya desain dapur sederhana di kampung yang unik.",
+  ParagraphTwo:
+    "Dapur sederhana akan terlihat aesthetic dan berfungsi dengan baik, bila kamu jeli menata dekorasinya. Keinginan mewujudkan dapur minimalis modern ukuran kecil tapi cantik, bisa kamu mulai dari bagian dinding.Bisa dimulai memilih warna cat dinding terbaik, hingga menggantung hiasan guna melengkapi dekorasi dapur dapur sederhana. Namun, untuk bikin desain dapur cantik, tidak harus selalu mengandalkan warna cat. Kamu dapat mengeksplorasi desain dapur minimalis 3x2 meter terlihat menarik dengan mengaplikasikan dinding batu bata yang diekspos.Langkah itu pastinya, bakal mendatangkan suasana selaiknya desain dapur sederhana di kampung yang unik.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Design",
+  paragraphOne:
+    "Dapur sederhana akan terlihat aesthetic dan berfungsi dengan baik, bila kamu jeli menata dekorasinya. Keinginan mewujudkan dapur minimalis modern ukuran kecil tapi cantik, bisa kamu mulai dari bagian dinding.Bisa dimulai memilih warna cat dinding terbaik, hingga menggantung hiasan guna melengkapi dekorasi dapur dapur sederhana. Namun, untuk bikin desain dapur cantik, tidak harus selalu mengandalkan warna cat. Kamu dapat mengeksplorasi desain dapur minimalis 3x2 meter terlihat menarik dengan mengaplikasikan dinding batu bata yang diekspos.Langkah itu pastinya, bakal mendatangkan suasana selaiknya desain dapur sederhana di kampung yang unik.",
+  ParagraphTwo:
+    "Dapur sederhana akan terlihat aesthetic dan berfungsi dengan baik, bila kamu jeli menata dekorasinya. Keinginan mewujudkan dapur minimalis modern ukuran kecil tapi cantik, bisa kamu mulai dari bagian dinding.Bisa dimulai memilih warna cat dinding terbaik, hingga menggantung hiasan guna melengkapi dekorasi dapur dapur sederhana. Namun, untuk bikin desain dapur cantik, tidak harus selalu mengandalkan warna cat. Kamu dapat mengeksplorasi desain dapur minimalis 3x2 meter terlihat menarik dengan mengaplikasikan dinding batu bata yang diekspos.Langkah itu pastinya, bakal mendatangkan suasana selaiknya desain dapur sederhana di kampung yang unik.",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,84 @@
+import ImageOne from "../images/house-1.jpg";
+import ImageTwo from "../images/house-2.jpg";
+import ImageThree from "../images/house-3.jpg";
+import ImageFour from "../images/house-4.jpg";
+import ImageFive from "../images/house-5.jpg";
+import ImageSix from "../images/house-6.jpg";
+import ImageSeven from "../images/house-7.jpg";
+import ImageEight from "../images/house-8.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,288,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+
+  {
+    title: "House in San Jose, Costa Rica",
+    price: "$2,868,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+
+  {
+    title: "House in Raja Ampat, Lombok",
+    price: "$3,100,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+
+  {
+    title: "House in Calabasas, California",
+    price: "$2,288,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
+
+export const SliderDataTwo = [
+  {
+    title: "House in Houston, Texas",
+    price: "$5,300,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFive,
+    alt: "House",
+  },
+
+  {
+    title: "House in Las Vegas, Nevada",
+    price: "$3,000,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageSix,
+    alt: "House",
+  },
+
+  {
+    title: "House in Bali, Denpasar",
+    price: "$1,288,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageSeven,
+    alt: "House",
+  },
+
+  {
+    title: "Luxury Villa in Dubai, Dubai",
+    price: "$6,288,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageEight,
+    alt: "House",
+  },
+];
